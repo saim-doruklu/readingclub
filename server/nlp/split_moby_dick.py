ef59f75e74e4b9351f23334146e3e27f76a16377
saim <saim.doruklu@bilgiedu.net>
2025-05-18 13:16:09 +0200

    working

---
+++ server/nlp/split_moby_dick.py
@@ -0,0 +1,18 @@
+from nltk.tokenize import sent_tokenize
+
+if __name__ == '__main__':
+    occured = False
+    start_parsing = False
+    with open("D:/PycharmProjects/ReadingGroup/resources/books/moby_dick", "rb") as file:
+        as_string = file.read().decode('utf-8')
+        tokens = sent_tokenize(as_string)
+        tokens_with_newlines_removed = [item.replace("\r\n", " ") for item in tokens]
+        for token in tokens_with_newlines_removed:
+            if start_parsing is False:
+                if "Loomings." in token:
+                    if occured is False:
+                        occured = True
+                    else:
+                        start_parsing = True
+            else:
+                print("Printing proper")
