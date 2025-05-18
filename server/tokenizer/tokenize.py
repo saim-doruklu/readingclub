d8a7467fadbcf5c6d9a77b47681c3f649ff0c98d
saim <saim.doruklu@bilgiedu.net>
2025-05-18 15:57:44 +0200

    remove spaces

--- server/tokenizer/tokenize.py
+++ server/tokenizer/tokenize.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     book_name = sys.argv[1:][0]
     occured = False
     start_parsing = False
-    with open(f'{books_root}/{book_name}/raw', "rb") as untokenized_book:
-        with open(f'{books_root}/{book_name}/tokenized', "w", encoding='utf-8') as tokenized_book:
-            as_string = untokenized_book.read().decode('utf-8')
+    with open(f'{books_root}/{book_name}/raw', "rb") as raw:
+        with open(f'{books_root}/{book_name}/tokenized', "w", encoding='utf-8') as tokenized:
+            as_string = raw.read().decode('utf-8')
             tokens = sent_tokenize(as_string)
             tokens_with_newlines_removed = [item.replace("\r\n", " ") for item in tokens]
             for token in tokens_with_newlines_removed:
@@ -20,5 +20,5 @@ if __name__ == '__main__':
                         else:
                             start_parsing = True
                 else:
-                    tokenized_book.write(token)
-                    tokenized_book.write("\r\n")
+                    tokenized.write(token)
+                    tokenized.write("\r\n")
